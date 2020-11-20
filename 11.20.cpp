b8c3423fca5f3b46cef74b8ed5d3c3065f653f73
1409883630 <1409883630@qq.com>
2020-11-20 23:37:30 +0800

    求圆的周长，面积

---
+++ 11.20.cpp
@@ -0,0 +1,18 @@
+//该程序让用户输入圆的半径Radius，输出圆的周长Perimeter和面积Area
+//Ex1_02.CPP
+#include <iostream>
+#include <cmath>
+using namespace std;
+const double PI = 3.14159;	//定义一个常量PI  #define PI 3.14159
+int main()
+{
+	int radius;
+	double perimeter, area;
+	cout << "请输入半径: ";
+	cin >> radius;
+	perimeter = 2 * PI * radius;					//周长=2πR
+	area = PI * pow(radius, 2);					//面积=πR2
+	cout << "周长是 : " << perimeter << endl;
+	cout << "面积是 : " << area << endl;
+	return 0;
+}
