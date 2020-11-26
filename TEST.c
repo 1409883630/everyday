7d49095cf5de15460948717e00688418c5fa56cf
1409883630 <1409883630@qq.com>
2020-11-26 23:17:03 +0800

    两个数最大公约数

---
+++ TEST.c
@@ -0,0 +1,17 @@
+//给定两个数，求这两个数的最大公约数
+#define _CRT_SECURE_NO_WARNINGS 1
+#include<stdio.h>
+int main()
+{
+	int a;
+	int b;
+	int c;
+	scanf("%d %d", &a, &b);
+	while (c = a%b)
+	{
+		a = b;
+		b = c;
+	}
+	printf("%d\n", b);
+	return 0;
+}
