b26130d1e6893ac17398d6712c4eb33be6e2d0e9
1409883630 <1409883630@qq.com>
2020-11-27 12:58:28 +0800

    打印1 - 100之间所有3的倍数的数字

---
+++ test1.c
@@ -0,0 +1,13 @@
+//打印1 - 100之间所有3的倍数的数字
+#define _CRT_SECURE_NO_WARNINGS 1
+#include<stdio.h>
+int main()
+{
+	int i = 1;
+	for (i = 1; i <= 100; i++)
+	{
+		if (i % 3 == 0)
+			printf("%d\n", i);
+	}
+	return 0;
+}
