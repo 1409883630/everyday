1e8e97d8c29a047e38192a110a989c15fdefef07
1409883630 <1409883630@qq.com>
2020-11-27 13:34:25 +0800

    打印1000年到2000年之间的闰年

--- test3.c
+++ test3.c
@@ -6,7 +6,7 @@ int main()
 	int i = 1000;
 	for (i = 1000; i <= 2000; i++)
 	{
-		if (i % 4 == 0 && i % 100 != 0)
+		if (i % 4 == 0 && i % 100 != 0 || i % 400 == 0)
 			printf("%d\n", i);
 	}
 	return 0;
