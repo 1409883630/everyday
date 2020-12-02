1040eafc3bac183e1d0363dc01c04c8f3f41df9b
1409883630 <1409883630@qq.com>
2020-12-02 22:52:56 +0800

    求数组中10个整数中的最大值

---
+++ test11.c
@@ -0,0 +1,20 @@
+//求数组10个整数中的最大值
+#include<stdio.h>
+int main()
+{
+	int a[] = { 1, 0, 6, 5, 8, 7, 9, 3, 2, 4 };
+	int sum = a[0];
+	for (int i = 1; i <= 9; i++)
+	{
+		if (sum <= a[i])
+		{
+			sum = a[i];
+		}
+		else if (sum>a[i])
+		{
+			sum = sum;
+		}
+	}
+	printf("%d\n", sum);
+	return 0;
+}
