b747dea1205476b6160fd74371e0f893bb488143
1409883630 <74590401+1409883630@users.noreply.github.com>
2020-12-07 23:49:40 +0800

    Add files via upload

---
+++ test18.c
@@ -0,0 +1,20 @@
+//12*12乘法表
+#include<stdio.h>
+void add(int n)//构造一个函数
+{
+	for (int i = 1; i < n + 1; i++)//双重for循环打印
+	{
+		for (int j = 1; j <= i; j++)
+		{
+			printf("%d * %d = %2d ", j, i, i*j);
+		}
+		printf("\n");
+	}
+}
+int main()
+{
+	int a;
+	scanf("%d", &a);
+	add(a);//调用
+	return 0;
+}
