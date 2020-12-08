fdfe45447cb4a915e5be2b9e673135c2759b47a9
1409883630 <74590401+1409883630@users.noreply.github.com>
2020-12-08 19:12:17 +0800

    非递归求n的阶乘

---
+++ test19.c
@@ -0,0 +1,18 @@
+//·ÇµÝ¹éÇónµÄ½×³Ë
+#include<stdio.h>
+void add(int n)
+{
+	int ret = 1;
+	for (int i = 2; i <= n; i++)
+	{
+		ret *= i;
+	}
+	printf("%d\n", ret);
+}
+int main()
+{
+	int a;
+	scanf("%d", &a);
+	add(a);
+	return 0;
+}
