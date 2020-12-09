b8834d9594eb626c14aba3cc3a7124227557ae65
1409883630 <74590401+1409883630@users.noreply.github.com>
2020-12-09 22:58:58 +0800

    递归求n的阶乘

---
+++ test20.c
@@ -0,0 +1,19 @@
+//µİ¹éÇónµÄ½×³Ë
+#include<stdio.h>
+int ret=1;
+int add(int n)
+{	
+	if (n >= 2)
+	{
+		ret *= n;
+		return add(n - 1);
+	}	  
+}
+int main()
+{
+	int a;
+	scanf("%d", &a);
+	add(a);
+	printf("%d\n", ret);
+	return 0;
+}
