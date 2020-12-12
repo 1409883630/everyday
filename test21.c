8907e77138a81fcaf4d426b8ca394eeb7137578c
1409883630 <74590401+1409883630@users.noreply.github.com>
2020-12-12 23:25:55 +0800

    计算一个数的每位之和（递归实现）

---
+++ test21.c
@@ -0,0 +1,22 @@
+//计算一个数的每位之和（递归实现）
+#include<stdio.h>
+int  DigitSum(int n)
+{
+	int sum = 0;
+	if (n < 10)
+	{
+		return n;
+	}
+	else
+	{
+		return sum = n % 10 + DigitSum(n / 10);
+	}
+}
+int main()
+{
+	int a;
+	scanf("%d", &a);
+	DigitSum(a);
+	printf("%d\n", DigitSum(a));
+	return 0;
+}
