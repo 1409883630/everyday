c720ff41134776e5ae0758f7da2a3feb1d5c955a
1409883630 <74590401+1409883630@users.noreply.github.com>
2020-12-14 23:32:58 +0800

    编写一个函数实现n的k次方，使用递归实现

---
+++ test23.c
@@ -0,0 +1,20 @@
+//编写一个函数实现n的k次方，使用递归实现。
+#include<stdio.h>
+int sqrt(int n,int k)
+{
+	if (k ==  0)
+	{
+		return 1;
+	}
+	else if (k>=1)
+	{
+		return n*sqrt(n, k - 1);
+	}
+}
+int main()
+{
+	int a,b;
+	scanf("%d %d", &a, &b);
+	printf("%d\n", sqrt(a, b));
+	return 0;
+}
