311e4b7385f6a01db0c3d248518ff1b0e4e4e358
1409883630 <74590401+1409883630@users.noreply.github.com>
2020-12-17 23:22:44 +0800

    递归实现1+2+..+n;

---
+++ test26.c
@@ -0,0 +1,17 @@
+//µİ¹éÊµÏÖ1+2+..+n;
+#include<stdio.h>
+int add(int n)
+{
+	if (n == 1)
+	{
+		return 1;
+	}
+	return n + add(n - 1);
+}
+int main()
+{
+	int a;
+	scanf("%d", &a);
+	printf("%d\n", add(a));
+		return 0;
+}
