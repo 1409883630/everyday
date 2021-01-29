7d2fe3cc30555b18b1e2584f0a268a454a194f33
1409883630 <74590401+1409883630@users.noreply.github.com>
2021-01-29 23:57:21 +0800

    写一个递归方法，输入一个非负整数，返回组成它的数字之和

---
+++ test33.java
@@ -0,0 +1,28 @@
+package com.company;
+//写一个递归方法，输入一个非负整数，返回组成它的数字之和
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static int fan(int n){
+        int sum=0;
+        if(n<10)
+        {
+            return n;
+        }
+        else
+        {
+            return sum=n%10+fan(n/10);
+        }
+    }
+
+    public static void main(String[] args) {
+        Scanner sca =new Scanner(System.in);
+        int a = sca.nextInt();
+        System.out.println(fan(a));
+
+
+
+    }
+}
