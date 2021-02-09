dea459c2ca4efe768a90bd8520404cd4142c4326
1409883630 <74590401+1409883630@users.noreply.github.com>
2021-02-09 23:03:02 +0800

    实现方法 contains, 能够判定字符串中是否包含子串

---
+++ test35.java
@@ -0,0 +1,9 @@
+package com.company;
+//实现方法 contains, 能够判定字符串中是否包含子串
+public class contains {
+    public static void main(String[] args) {
+        String str = "helloword";
+        System.out.println(str.contains("hello"));
+        System.out.println(str.contains("helow"));
+    }
+}
