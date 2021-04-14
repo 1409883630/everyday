8d6257d0b40140e3e5641aa47c1e3f9db9901822
1409883630 <74590401+1409883630@users.noreply.github.com>
2021-04-14 16:55:21 +0800

    数组中出现次数超过一半的数

---
+++ day50.java
@@ -0,0 +1,20 @@
+ //数组中出现次数超过一半的数
+   
+    public static int MoreThanHalfNum_Solution(int[] array) {
+        if(array.length==0){
+            return 0;
+        }
+        for (int i = 0; i < array.length; i++) {
+            int n = array.length;
+            int count = 0;//通过判断出现次数来解决
+            for (int j = 0; j < array.length; j++) {
+                if (array[i] == array[j]) {
+                    count++;
+                }
+            }
+            if (count > n / 2) {
+                return array[i];
+            }
+        }
+       return 0;
+    }
