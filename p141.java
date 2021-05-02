5f0935061aaf54d0d8be0db08feb47138d17aee6
1409883630 <74590401+1409883630@users.noreply.github.com>
2021-05-02 16:22:28 +0800

    给定一个链表，判断链表中是否有环

---
+++ p141.java
@@ -0,0 +1,19 @@
+//给定一个链表，判断链表中是否有环
+
+public class Solution {
+    public boolean hasCycle(ListNode head) {
+        if(head==null){
+            return false;
+        }
+        ListNode a= head;
+        ListNode b=head;
+        while(a!=null&&a.next!=null){
+            a=a.next.next;
+            b=b.next;
+            if(a==b){
+                return true;
+            }
+        }
+        return false;
+    }
+}
