ed6247814c7e3dffc2e732cd98a2aa83e9899721
1409883630 <74590401+1409883630@users.noreply.github.com>
2021-05-17 22:29:41 +0800

    数根

---
+++ day14.java
@@ -0,0 +1,17 @@
+//Êı¸ù
+import java.util.*;
+public class Main{
+  public static void main(String[] args){
+        Scanner sc=new Scanner(System.in);
+        while(sc.hasNext()){
+            String str=sc.next();
+            while(str.length()>1){
+                int count=0;
+                for(int i=0;i<str.length();i++)
+                    count+=str.charAt(i)-'0';
+                str=String.valueOf(count);
+            }
+            System.out.println(str);
+        }
+    }
+}
