93f159f7aeec900b9412eafca5e88d3859cae6ad
1409883630 <74590401+1409883630@users.noreply.github.com>
2021-05-18 22:54:50 +0800

    快到碗里来

---
+++ day15.java
@@ -0,0 +1,16 @@
+  //快到碗里来
+  import java.util.*;
+  public class Test {
+  public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        while (sc.hasNextDouble()) {
+            Double n = sc.nextDouble();
+            Double r = sc.nextDouble();
+            if (n <= 6.28 * r) {
+                System.out.println("Yes");
+            } else {
+                System.out.println("No");
+            }
+        }
+    }
+	}
