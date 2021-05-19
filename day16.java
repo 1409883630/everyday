2c08efe0adb61a78700b9f09093364d21a741286
1409883630 <74590401+1409883630@users.noreply.github.com>
2021-05-19 23:15:58 +0800

    不用加减乘除做加法

---
+++ day16.java
@@ -0,0 +1,16 @@
+
+
+ //不用加减乘除做加法
+ 
+ import java.util.*;
+ public class Test {
+    public int Add(int num1,int num2) {
+        //按位与或的方法
+        while(num2!=0){
+            int swap=num1^num2;
+            num2=(num1&num2)<<1;
+            num1=swap;
+        }
+        return num1;
+    }
+}
