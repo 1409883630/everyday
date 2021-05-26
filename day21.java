b85649447e88be788335cd1190e954ffb2161389
1409883630 <74590401+1409883630@users.noreply.github.com>
2021-05-26 22:37:46 +0800

    剪花布条

---
+++ day21.java
@@ -0,0 +1,25 @@
+ //¼ô»¨²¼Ìõ
+
+ import java.util.*;
+ public class Main{
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        while(sc.hasNext()){
+            String s= sc.next();
+            String t= sc.next();
+            System.out.println(ss(s,t));
+        }
+    }
+    public static int ss(String s1,String s2){
+        int n=0;
+
+        for(int i=0;i<=s1.length()-s2.length();i++){
+            int j=i+s2.length();
+            if(s1.substring(i,j).equals(s2)){
+                n++;
+                i=j-1;
+            }
+        }
+        return n;
+  }
+ }
