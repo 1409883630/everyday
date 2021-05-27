baf8a1853a9164a742567ec5ee38c519c42dd13f
1409883630 <74590401+1409883630@users.noreply.github.com>
2021-05-27 20:31:25 +0800

    养兔子

---
+++ day22.java
@@ -0,0 +1,17 @@
+// ÑøÍÃ×Ó
+import java.util.*;
+public class Main{
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        while (sc.hasNextInt()) {
+            int n = sc.nextInt();
+            long[] a = new long[91];
+            a[0] = 1;
+            a[1] = 2;
+            for (int i = 2; i <= 90; i++) {
+                a[i] = a[i - 1] + a[i - 2];
+            }
+            System.out.println(a[n - 1]);
+        }
+    }
+}
