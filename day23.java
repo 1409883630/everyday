c5a09c85db85278b38019d677093df32601dd41e
1409883630 <74590401+1409883630@users.noreply.github.com>
2021-05-28 22:31:30 +0800

    年会抽奖

---
+++ day23.java
@@ -0,0 +1,34 @@
+//Äê»á³é½±
+
+import java.util.*;
+public class Main{
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        while (sc.hasNextInt()) {
+            int n = sc.nextInt();
+            if (n >= 2 && n <= 20) {
+                double a = ss(n);
+                double b = aa(n);
+                double c = (a / b) * 100;
+                System.out.println(String.format("%.2f",c)+"%");
+            } else {
+                break;
+            }
+        }
+    }
+    public static double ss(int n) {
+        if (n == 1) {
+            return 0;
+        } else if (n == 2) {
+            return 1;
+        }
+        //´íÅÅ
+            return (n - 1) * (ss(n - 1) + ss(n - 2));
+    }
+    public static double aa(int n) {
+        if (n == 1) {
+            return 1;
+        }
+           return n * aa(n - 1);
+    }
+}
