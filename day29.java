1a840ea6adda3c6df02e19eb92b3b342bfb2a27e
1409883630 <74590401+1409883630@users.noreply.github.com>
2021-06-04 14:51:52 +0800

    发邮件

---
+++ day29.java
@@ -0,0 +1,19 @@
+
+ //·¢ÓÊ¼þ
+ 
+import java.util.*;
+ public class Main{
+    public static void main(String[]args){
+        Scanner sc=new Scanner(System.in);
+        while(sc.hasNextInt()){
+            int n=sc.nextInt();
+           long[]a=new long[21];
+           a[0]=0;
+           a[1]=1;
+           for(int i=2;i<=20;i++){
+               a[i]=i*(a[i-1]+a[i-2]);
+           }
+            System.out.println(a[n-1]);
+            }
+        }
+ }
