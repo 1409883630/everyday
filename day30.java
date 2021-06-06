c2cea1a59011a8c3cd761f41daca2fbaa6eedb34
1409883630 <74590401+1409883630@users.noreply.github.com>
2021-06-06 21:32:17 +0800

    Add files via upload

---
+++ day30.java
@@ -0,0 +1,20 @@
+
+ //Ω‚∂¡√‹¬Î
+ import java.util.*;
+ public class Main{
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        while(sc.hasNext()){
+
+            String s=sc.nextLine();
+             String count ="";
+            int n=s.length();
+            for(int i=0;i<n;i++){
+                if(s.charAt(i)>='0'&&s.charAt(i)<='9'){
+                    count=count + s.charAt(i);
+                }
+            }
+            System.out.println(count);
+        }
+    }
+ }
