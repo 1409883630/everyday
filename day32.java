5d42221275667654896c0eac453677a5fbaa8d43
1409883630 <74590401+1409883630@users.noreply.github.com>
2021-06-09 21:35:57 +0800

    单词倒排

---
+++ day32.java
@@ -0,0 +1,15 @@
+ //µ•¥ µπ≈≈
+ import java.util.*;
+ public class Main{
+	    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+
+        String a=sc.nextLine();
+        String[] arr=a.split("[^a-zA-Z]+");
+        List<String> list=Arrays.asList(arr);
+        Collections.reverse(list);
+        String b=String.join(" ",list);
+        System.out.println(b);
+    }
+	 
+ }
