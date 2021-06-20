8fd4d4e2a562bfc0fd0ff52218baed30b6b4599f
1409883630 <74590401+1409883630@users.noreply.github.com>
2021-06-20 22:12:08 +0800

    求整数1~100的累加值，但要求跳过所有个位为3的数

---
+++ day39.java
@@ -0,0 +1,17 @@
+//求整数1~100的累加值，但要求跳过所有个位为3的数。（使用for循环实现）
+
+import java.util.Scanner;
+public class Demo03 {
+	public static void main(String[] args) {
+		int sum=0;
+		for(int i=1;i<=100;i++) {
+			int a=i%10;
+			if(a!=3) {
+				System.out.println(i);
+				sum +=i;
+			}
+		}
+		System.out.println(sum);
+	   
+} 
+	}
