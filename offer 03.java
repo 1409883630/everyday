ed05205fe9d7e9c28302b1a7624f64c27ebf6ae7
1409883630 <74590401+1409883630@users.noreply.github.com>
2021-06-29 23:37:40 +0800

    数组中重复的数字

---
+++ offer 03.java
@@ -0,0 +1,18 @@
+//数组中重复的数字
+
+class Solution {
+    public int findRepeatNumber(int[] nums) {
+        int i = 0;
+        while(i < nums.length) {
+            if(nums[i] == i) {
+                i++;
+                continue;
+            }
+            if(nums[nums[i]] == nums[i]) return nums[i];
+            int tmp = nums[i];
+            nums[i] = nums[tmp];
+            nums[tmp] = tmp;
+        }
+        return -1;
+    }
+}
