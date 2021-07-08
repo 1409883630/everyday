5a9e263bdbb48d5a0aaf6711282eb4d0774e4598
1409883630 <74590401+1409883630@users.noreply.github.com>
2021-07-08 22:54:46 +0800

    二叉树的镜像

---
+++ offer 27.java
@@ -0,0 +1,10 @@
+//¶þ²æÊ÷µÄ¾µÏñ
+class Solution {
+    public TreeNode mirrorTree(TreeNode root) {
+        if(root == null) return null;
+        TreeNode tmp = root.left;
+        root.left = mirrorTree(root.right);
+        root.right = mirrorTree(tmp);
+        return root;
+    }
+}
