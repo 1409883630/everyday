4b82ad8412fdf09028fc5d4c72f1e6e5bb826ee5
1409883630 <74590401+1409883630@users.noreply.github.com>
2021-07-13 22:48:00 +0800

    因子的个数

--- day18.java
+++ day18.java
@@ -1,6 +1,6 @@
 //因子的个数
 public class Main{
-    public void main（String [] args）{
+    public static void main（String [] args）{
         Scanner sc=new Scanner(System.in);
         while(sc.hasNextInt()){
             int n=sc.nextInt();
