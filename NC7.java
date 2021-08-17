1a6ac613a44a1e7c921125734e7e715f2e2fe022
1409883630 <74590401+1409883630@users.noreply.github.com>
2021-08-17 23:10:09 +0800

    NC7 买卖股票的最好时机

--- NC7.java
+++ NC7.java
@@ -1,32 +1,32 @@
-//NC7 ������Ʊ�����ʱ��
-//��������һ�����飬���е�\ i i ��Ԫ���ǹ�Ʊ�ڵ�\ i i ��ļ۸�
-//����һ������������Ļ��ᡣ��ֻ�������˹�Ʊ�Ժ������������
-//�������һ���㷨��������Ի�õ�������档
+//NC7 买卖股票的最好时机
+//假设你有一个数组，其中第\ i i 个元素是股票在第\ i i 天的价格。
+//你有一次买入和卖出的机会。（只有买入了股票以后才能卖出）。
+//请你设计一个算法来计算可以获得的最大收益
 import java.util.*;
 public class Solution {
     /**
      * 
-     * @param prices int����һά���� 
-     * @return int����
+     * @param prices int整型一维数组 
+     * @return int整型
      */
     public int maxProfit (int[] prices) {
         // write code here
         // write code here
         int len = prices.length;
-        // �����ж�
+        // 特殊判断
         if (len < 2) {
             return 0;
         }
         int[][] dp = new int[len][2];
  
-        // dp[i][0] �±�Ϊ i ���������ʱ�򣬲��ֹɣ�����ӵ�е��ֽ���
-        // dp[i][1] �±�Ϊ i ���������ʱ�򣬳ֹɣ�����ӵ�е��ֽ���
+        // dp[i][0] 下标为 i 这天结束的时候，不持股，手上拥有的现金数
+        // dp[i][1] 下标为 i 这天结束的时候，持股，手上拥有的现金数
  
-        // ��ʼ�������ֹ���ȻΪ 0���ֹɾ���Ҫ��ȥ�� 1 �죨�±�Ϊ 0���Ĺɼ�
+        // 初始化：不持股显然为 0，持股就需要减去第 1 天（下标为 0）的股价
         dp[0][0] = 0;
         dp[0][1] = -prices[0];
  
-        // �ӵ� 2 �쿪ʼ����
+        // 从第 2 天开始遍历
         for (int i = 1; i < len; i++) {
             dp[i][0] = Math.max(dp[i - 1][0], dp[i - 1][1] + prices[i]);
             dp[i][1] = Math.max(dp[i - 1][1], -prices[i]);
