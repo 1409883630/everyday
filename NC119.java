3dd24ea6ed07add17af51c86bfbc7e4d7c72e04f
1409883630 <74590401+1409883630@users.noreply.github.com>
2021-08-24 23:27:13 +0800

    NC119 最小的K个数

---
+++ NC119.java
@@ -0,0 +1,26 @@
+//NC119 最小的K个数
+//给定一个数组，找出其中最小的K个数。例如数组元素是4,5,1,6,2,7,3,8这8个数字，
+//则最小的4个数字是1,2,3,4。
+//0 <= k <= input.length <= 10000
+//0 <= input[i] <= 10000
+import java.util.ArrayList;
+
+public class Solution {
+    public ArrayList<Integer> GetLeastNumbers_Solution(int [] input, int k) {
+        ArrayList<Integer> al = new ArrayList<Integer>();
+        if (k > input.length) {
+            return al;
+        }
+        for (int i = 0; i < k; i++) {
+            for (int j = 0; j < input.length - i - 1; j++) {
+                if (input[j] < input[j + 1]) {
+                    int temp = input[j];
+                    input[j] = input[j + 1];
+                    input[j + 1] = temp;
+                }
+            }
+            al.add(input[input.length - i - 1]);
+        }
+        return al;
+    }
+}
