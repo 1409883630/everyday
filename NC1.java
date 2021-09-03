4d3421036793ee22132a15bcf0d7d101f88c017b
1409883630 <74590401+1409883630@users.noreply.github.com>
2021-09-03 21:07:42 +0800

    NC1 大数加法

---
+++ NC1.java
@@ -0,0 +1,28 @@
+//NC1 大数加法
+//以字符串的形式读入两个数字，编写一个函数计算它们的和，以字符串形式返回。
+//（字符串长度不大于100000，保证字符串仅由'0'~'9'这10种字符组成）
+import java.util.*;
+public class Solution {
+    /**
+     * 代码中的类名、方法名、参数名已经指定，请勿修改，直接返回方法规定的值即可
+     * 计算两个数之和
+     * @param s string字符串 表示第一个整数
+     * @param t string字符串 表示第二个整数
+     * @return string字符串
+     */
+    public String solve (String s, String t) {
+        // write code here
+        Stack<Integer> stack = new Stack<>();
+        StringBuilder stringBuilder = new StringBuilder();
+        int i = s.length() - 1, j = t.length() - 1, carry = 0;
+        while (i >= 0 || j >= 0 || carry != 0) {
+            carry += i >= 0 ? s.charAt(i--) - '0' : 0;
+            carry += j >= 0 ? t.charAt(j--) - '0' : 0;
+            stack.push(carry % 10);
+            carry = carry / 10;
+        }
+        while (!stack.isEmpty())
+            stringBuilder.append(stack.pop());
+        return stringBuilder.toString();
+    }
+}
