a1f84735f2fafab5bab6c3c18382473d97ddb772
1409883630 <74590401+1409883630@users.noreply.github.com>
2021-09-15 22:50:34 +0800

    NC61 两数之和

---
+++ NC61.java
@@ -0,0 +1,26 @@
+//NC61 两数之和
+//给出一个整数数组 numbers 和一个目标值 target，请在数组中找出两个加起来等于目标值的数，
+//你给出的函数twoSum 需要返回这两个数字的下标（index1，index2）组成的数组，
+//需要满足 index1 小于index2.。注意：下标是从1开始的
+//保证给出的数组中只存在唯一解。
+import java.util.*;
+public class Solution {
+    /**
+     * 
+     * @param numbers int整型一维数组 
+     * @param target int整型 
+     * @return int整型一维数组
+     */
+    public int[] twoSum (int[] numbers, int target) {
+        // write code here
+         for(int i = 0; i<numbers.length; i++){
+             for(int j = i+1; j<numbers.length; j++){
+                 if(numbers[i] + numbers[j] == target){
+                     return new int[]{i+1, j+1};
+                     
+    }
+      }
+         }
+         return new int[0];
+    }
+     }
