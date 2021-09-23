faf71200b5ec9c69db80053061d51a3180deec91
&可乐 <74590401+1409883630@users.noreply.github.com>
2021-09-23 23:56:52 +0800

    NC65 斐波那契数列

---
+++ NC65.java
@@ -0,0 +1,18 @@
+//NC65 ì³²¨ÄÇÆõÊıÁĞ
+public class Solution {
+    public int Fibonacci(int n){
+        if(n==0) 
+         return 0;
+        if(n==1) 
+          return 1;
+        int a=0;
+        int b=1;
+        int sum=0;
+        for(int i=2;i<=n;i++){
+            sum=a+b;
+            a=b;
+            b=sum;
+        }
+        return sum;
+    }
+}
