330e559df94b69421844d91a4e421dc177d9f78c
&可乐 <74590401+1409883630@users.noreply.github.com>
2021-09-25 23:17:02 +0800

    NC133 链表的奇偶重排

---
+++ NC133.java
@@ -0,0 +1,19 @@
+//NC133 链表的奇偶重排
+//给定一个单链表，请设定一个函数，将链表的奇数位节点和偶数位节点分别放在一起，重排后输出。
+//注意是节点的编号而非节点的数值。
+import java.util.*;
+public class Solution {
+    public ListNode oddEvenList (ListNode head) {
+        if(head == null || head.next == null) return head;
+        ListNode evenHead = head.next;
+        ListNode odd = head,even = head.next;
+        while(even != null && even.next != null){
+            odd.next = even.next;
+            odd = odd.next;
+            even.next = odd.next;
+            even = even.next;
+        }
+        odd.next = evenHead;
+        return head;
+    }
+}
