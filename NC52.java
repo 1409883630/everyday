3da794a5f805ed89a9cc4b6d38b6c0c4cad6d78b
&可乐 <74590401+1409883630@users.noreply.github.com>
2021-09-29 23:08:20 +0800

    NC52 括号序列

---
+++ NC52.java
@@ -0,0 +1,18 @@
+//NC52 括号序列
+//给出一个仅包含字符'(',')','{','}','['和']',的字符串，判断给出的字符串是否是合法的括号序列
+//括号必须以正确的顺序关闭，"()"和"()[]{}"都是合法的括号序列，但"(]"和"([)]"不合法。
+import java.util.*;
+public class Solution {
+    public boolean isValid (String s) {
+        // write code here
+        Stack<Character> stack = new Stack<>();
+        char[] temp = s.toCharArray();
+        for (char c : temp){
+            if (c == '(') stack.push(')');
+            else if (c == '[') stack.push(']');
+            else if (c == '{') stack.push('}');
+            else if (stack.isEmpty() || stack.pop() != c) return false;
+        }
+        return stack.isEmpty();
+    }
+}
