25b4522592dcca9ba3e7f940a7529a791f884814
&可乐 <74590401+1409883630@users.noreply.github.com>
2021-10-06 22:32:39 +0800

    30. 串联所有单词的子串

--- p30.java
+++ p30.java
@@ -1,8 +1,8 @@
-//30. �������е��ʵ��Ӵ�
-//����һ���ַ��� s ��һЩ ������ͬ �ĵ��� words ��
-//�ҳ� s ��ǡ�ÿ����� words �����е��ʴ����γɵ��Ӵ�����ʼλ�á�
-//ע���Ӵ�Ҫ�� words �еĵ�����ȫƥ�䣬�м䲻���������ַ� ��
-//������Ҫ���� words �е��ʴ�����˳��
+//30. 串联所有单词的子串
+//给定一个字符串 s 和一些 长度相同 的单词 words 。
+//找出 s 中恰好可以由 words 中所有单词串联形成的子串的起始位置。
+//注意子串要与 words 中的单词完全匹配，中间不能有其他字符 ，
+//但不需要考虑 words 中单词串联的顺序，正反都可以
 class Solution {
     public List<Integer> findSubstring(String s, String[] words) {
         List<Integer> res = new ArrayList<>();
