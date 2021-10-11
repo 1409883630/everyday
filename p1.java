1d6be65aba927304ab359284d6fad1a120dd5a63
&可乐 <74590401+1409883630@users.noreply.github.com>
2021-10-11 23:09:23 +0800

    两数之和

---
+++ p1.java
@@ -0,0 +1,19 @@
+//两数之和
+//给定一个整数数组 nums 和一个整数目标值 target，
+//请你在该数组中找出 和为目标值 target  的那 两个 整数，并返回它们的数组下标。
+class Solution {
+    public int[] twoSum(int[] nums, int target) {
+        int[] a=new int[2];
+         for(int i=0;i<nums.length;i++){
+             int res=nums[i];
+             for(int j=i+1;j<nums.length;j++){
+                 if(res+nums[j]==target){
+                     a[0]=i;
+                     a[1]=j;
+                      return a;
+             }
+             }  
+         }
+         return a;
+	}
+}
