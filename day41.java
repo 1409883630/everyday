92a01b146ebd24bf010befd0c585f7c558c0eb9a
&可乐 <74590401+1409883630@users.noreply.github.com>
2021-10-12 23:52:26 +0800

    获取字符串长度

---
+++ day41.java
@@ -0,0 +1,11 @@
+//获取字符串长度
+//键盘输入一个字符串，编写代码获取字符串的长度并输出，要求使用字符指针实现。
+import java.util.*;
+public class Main{
+    public static void main(String[] args){
+        Scanner sc=new Scanner(System.in);
+        String str = sc.nextLine();
+        int s=str.length();
+        System.out.println(s);
+}
+}
