897dedb23f76d85200ca528abd00790543a99f86
&可乐 <74590401+1409883630@users.noreply.github.com>
2021-10-17 23:58:46 +0800

    13. 罗马数字转整数

---
+++ p13.java
@@ -0,0 +1,36 @@
+//13. 罗马数字转整数
+//罗马数字包含以下七种字符: I， V， X， L，C，D 和 M。
+//字符          数值
+//I             1
+//V             5
+//X             10
+//L             50
+//C             100
+//D             500
+//M             1000
+class Solution {
+    Map<Character, Integer> symbolValues = new HashMap<Character, Integer>() {{
+        put('I', 1);
+        put('V', 5);
+        put('X', 10);
+        put('L', 50);
+        put('C', 100);
+        put('D', 500);
+        put('M', 1000);
+    }};
+
+    public int romanToInt(String s) {
+        int ans = 0;
+        int n = s.length();
+        for (int i = 0; i < n; ++i) {
+            int value = symbolValues.get(s.charAt(i));
+            if (i < n - 1 && value < symbolValues.get(s.charAt(i + 1))) {
+                ans -= value;
+            } else {
+                ans += value;
+            }
+        }
+        return ans;
+    }
+}
+
