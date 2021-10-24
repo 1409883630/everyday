dd36aca684bf26844bb890ccc0faef91e9c29bdb
&可乐 <74590401+1409883630@users.noreply.github.com>
2021-10-24 23:53:32 +0800

    3. 无重复字符的最长子串

--- p3.java
+++ p3.java
@@ -1,15 +1,15 @@
-//3. ���ظ��ַ�����Ӵ�
-//����һ���ַ��� s �������ҳ����в������ظ��ַ��� ��Ӵ� �ĳ��ȡ�
+//3. 无重复字符的最长子串
+//给定一个字符串 s ，请你找出其中不含有重复字符的 最长子串 的长度。
 class Solution {
     public int lengthOfLongestSubstring(String s) {
-           // ��¼�ַ���һ�γ��ֵ�λ��
+         // 记录字符上一次出现的位置
         int[] last = new int[128];
         for(int i = 0; i < 128; i++) {
             last[i] = -1;
         }
         int n = s.length();
         int res = 0;
-        int start = 0; // ���ڿ�ʼλ��
+        int start = 0; // 窗口开始位置
         for(int i = 0; i < n; i++) {
             int index = s.charAt(i);
             start = Math.max(start, last[index] + 1);
